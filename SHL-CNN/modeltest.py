51ea577dfee763c085e3f32454bd1cdf29201a56
ialb97 <ialb998@gmail.com>
2018-10-22 22:41:30 -0400

    changed decay

--- SHL-CNN/modeltest.py
+++ SHL-CNN/modeltest.py
@@ -84,7 +84,7 @@ k2 = Dense(NUM_CLASSES_2,activation='softmax',kernel_initializer=keras.initializ
 model1 = Model(inputs=a, outputs=k1)
 model2 = Model(inputs=a, outputs=k2)
 
-optim = keras.optimizers.Adam(lr=.001,decay=.01)
+optim = keras.optimizers.Adam(lr=.001,decay=.001)
 
 model1.compile(loss=keras.losses.categorical_crossentropy,
             	optimizer=optim,
