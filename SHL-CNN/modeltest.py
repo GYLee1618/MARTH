dfe553e937ca386b47a2c11490d598920f0da246
ialb97 <ialb998@gmail.com>
2018-10-22 23:48:37 -0400

    lower val split

--- SHL-CNN/modeltest.py
+++ SHL-CNN/modeltest.py
@@ -62,10 +62,10 @@ x_test_2 /= 255
 # y_test_2 = keras.utils.to_categorical(y_test_2, NUM_CLASSES_EN)
 
 x_train_1, x_val_1, y_train_1, y_val_1 = train_test_split(
-	x_train_1,y_train_1,test_size=.1,random_state=random.seed(time.time()))
+	x_train_1,y_train_1,test_size=.05,random_state=random.seed(time.time()))
 
 x_train_2, x_val_2, y_train_2, y_val_2 = train_test_split(
-	x_train_2,y_train_2,test_size=.1,random_state=random.seed(time.time()))
+	x_train_2,y_train_2,test_size=.05,random_state=random.seed(time.time()))
 intial = keras.initializers.RandomNormal(mean=0, stddev=0.01,seed=random.seed(time.time()))
 
 
