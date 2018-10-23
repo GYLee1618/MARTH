bb647e534f10092c073d69d77e6f55c164f94539
ialb97 <ialb998@gmail.com>
2018-10-23 17:19:33 -0400

    testing

--- utils/get_images.py
+++ utils/get_images.py
@@ -5,8 +5,11 @@ def get_image(fname,size):
     im = Image.open(fname)
   
     im.thumbnail(size,Image.LANCZOS)
+   	if (fname == './ICDAR/train/char/1/100.jpg') :
+    	im.show()
     pix = im.getdata()
     pix = np.array(pix).reshape((im.size[0],im.size[1],3))
+
     result = np.zeros((size[0],size[1],3))
     result[:pix.shape[0],:pix.shape[1],:] = pix
     return result
