41b7da460009edb4a61714c7a0c65cc291047dbe
ialb97 <ialb998@gmail.com>
2018-10-23 00:02:40 -0400

    mom

--- SHL-CNN/modeltest.py
+++ SHL-CNN/modeltest.py
@@ -85,7 +85,7 @@ k2 = Dense(NUM_CLASSES_2,activation='softmax',kernel_initializer=intial)(j)
 model1 = Model(inputs=a, outputs=k1)
 model2 = Model(inputs=a, outputs=k2)
 
-optim = keras.optimizers.SGD(lr=.01,nesterov=True)
+optim = keras.optimizers.SGD(lr=.01,momentum=.1,nesterov=True)
 
 model1.compile(loss=keras.losses.categorical_crossentropy,
             	optimizer=optim,
