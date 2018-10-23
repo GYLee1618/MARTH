a40386456859a5e901e11ce5f11f88ae25f0689b
ialb97 <ialb998@gmail.com>
2018-10-23 12:09:13 -0400

    fixed end of line

--- SHL-CNN/modeltest.py
+++ SHL-CNN/modeltest.py
@@ -186,8 +186,8 @@ for ii in range(EPOCHS):
 		train1acc_sum += train1acc
 		train2error_sum += train2error
 		train2acc_sum += train2acc
-		print("Train1 loss: {:0.4f}\tTrain1 accuracy: {:0.4f}\tTrain2 loss: {:0.4f}\tTrain2 accuracy: {:0.4f}\r".format(
-			train1error_sum/(batch1_count+.0001),train1acc/(batch1_count+.0001),train2error/(batch2_count+.0001),train2acc/(batch2_count+.0001)))
+		print("Train1 loss: {:0.4f}\tTrain1 accuracy: {:0.4f}\tTrain2 loss: {:0.4f}\tTrain2 accuracy: {:0.4f}".format(
+			train1error_sum/(batch1_count+.0001),train1acc/(batch1_count+.0001),train2error/(batch2_count+.0001),train2acc/(batch2_count+.0001)),end='\r')
 	val1error,val1acc = model1.test_on_batch(x_val_1,y_val_1)
 	val2error,val2acc = model2.test_on_batch(x_val_2,y_val_2)
 	train1error = train1error_sum/num_batches
