197dc7951c43420ac0bcb41c907a1ec378caf476
ialb97 <ialb998@gmail.com>
2018-10-23 17:21:51 -0400

    k

--- utils/get_images.py
+++ utils/get_images.py
@@ -3,10 +3,9 @@ import numpy as np
 
 def get_image(fname,size):
     im = Image.open(fname)
-  
     im.thumbnail(size,Image.LANCZOS)
-	if (fname == './ICDAR/train/char/1/100.jpg'):
-		im.show()
+    if (fname == './ICDAR/train/char/1/100.jpg'):
+    	im.show()
     pix = im.getdata()
     pix = np.array(pix).reshape((im.size[0],im.size[1],3))
 
