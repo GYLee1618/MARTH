d44064fd987d44d169202d425f9439844948925f
ialb97 <ialb998@gmail.com>
2018-10-23 17:22:48 -0400

    i give up

--- utils/get_images.py
+++ utils/get_images.py
@@ -5,7 +5,7 @@ def get_image(fname,size):
     im = Image.open(fname)
     im.thumbnail(size,Image.LANCZOS)
     if (fname == './ICDAR/train/char/1/100.jpg'):
-    	im.show()
+    	im.save('./test.jpg','JPEG')
     pix = im.getdata()
     pix = np.array(pix).reshape((im.size[0],im.size[1],3))
 
