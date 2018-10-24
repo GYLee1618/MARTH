299fc7466e5d5de4e19559f76df92dbe2b5d81e6
gavin <horseantson@hotmail.com>
2018-10-24 08:53:41 -0400

    im dum

--- SHL-CNN/isaacmodelshl.py
+++ SHL-CNN/isaacmodelshl.py
@@ -199,8 +199,8 @@ for ii in range(EPOCHS):
     elif (rng > .5 and batch2_count < len(x_train_2_batches)):
       x_train_2_b,y_train_2_b = x_train_2_batches[batch2_count]
       history = model2.fit(x_train_2_b,y_train_2_b,epochs=1,verbose=0)
-      train1error_sum += history.history['loss'][0]
-      train1acc_sum += history.history['acc'][0]
+      train2error_sum += history.history['loss'][0]
+      train2acc_sum += history.history['acc'][0]
       batch2_count += 1
 
     else:
