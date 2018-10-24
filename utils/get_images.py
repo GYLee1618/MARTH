a5c461d485fd20a12a01567b9afffd336ff0ecfe
gavin <horseantson@hotmail.com>
2018-10-24 11:05:28 -0400

    gavin is a doof

--- utils/get_images.py
+++ utils/get_images.py
@@ -3,16 +3,19 @@ import numpy as np
 import time
 
 def get_image(fname,size):
-    im = Image.open(fname)
-    im.thumbnail(size,Image.LANCZOS)
-    # im.save('test'+str(time.time()),'JPEG')
-    pix = im.getdata()
-    pix = np.array(pix).reshape((im.size[0],im.size[1],3))
+	im = Image.open(fname)
+	im.thumbnail(size,Image.LANCZOS)
+	if fname == './ICDAR/test/char/28/2724.jpg':
+		im.save('test_'+fname.replace('/','-'),'JPEG')
+	pix = im.getdata()
+	pix = np.array(pix).reshape((im.size[1],im.size[0],3))
 
-    result = np.zeros((size[0],size[1],3))
-    rowpad = size[0]-pix.shape[0]
-    colpad = size[1]-pix.shape[1]
-    result[rowpad//2:rowpad//2+pix.shape[0],colpad//2:colpad//2+pix.shape[1],:] = pix
-
-    return result
+	result = np.zeros((size[0],size[1],3))
+	rowpad = size[0]-pix.shape[0]
+	colpad = size[1]-pix.shape[1]
+	result[rowpad//2:rowpad//2+pix.shape[0],colpad//2:colpad//2+pix.shape[1],:] = pix
+	image = Image.fromarray(result.astype('uint8'), 'RGB')
+	if fname == './ICDAR/test/char/28/2724.jpg':	
+		image.save('test'+fname.replace('/','-'),'JPEG')
+	return result
 
